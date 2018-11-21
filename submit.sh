06a76fd0e199882248ff29adbbccf3154390236a
Devdhar Patel <devdharpatel@gmail.com>
2018-11-21 15:29:19 -0500

    increase mem limit

--- submit.sh
+++ submit.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=ann
 #SBATCH --partition=1080ti-long
 #SBATCH --time=07-00:00:00
-#SBATCH --mem=64000
+#SBATCH --mem=256000
 #SBATCH --account=rkozma
 #SBATCH --output=res_%j.txt
 #SBATCH -e res_%j.err
